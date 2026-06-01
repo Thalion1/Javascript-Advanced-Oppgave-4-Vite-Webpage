4a290cc6fe9fbfb33ac8b7640906167ee842d180
Thalion1 <gustav.kodehode@gmail.com>
2026-06-01 10:25:40 +0200

    a

--- src/roleDise.js
+++
@@ -1,7 +0,0 @@
-import { randInt } from "pretty-random"
-
-export function roleDice(inputElement, displayElement) {
-    inputElement.addEventListener('click', () => {
-        displayElement.textContent = randInt(1, 6)
-    })
-}
